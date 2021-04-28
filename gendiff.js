5e9aa2531c7a0945574bc4bc679bf8dfcffb9d16
Timur Davlet <timazavr@mail.ru>
2021-04-28 23:37:03 +0300

    доработка справки

--- gendiff.js
+++ gendiff.js
@@ -9,5 +9,7 @@ const { program } = commander;
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
