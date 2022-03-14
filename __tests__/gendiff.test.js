235c7c1182269854a0b110a4abcfb38a8cf2416f
TimurDavlet <timkoootim@yandex.ru>
2022-03-14 14:56:54 +0400

    linter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-conditional-expect */
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { readFileSync } from 'fs';
@@ -10,27 +11,10 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const expectedComplex = readFileSync(getFixturePath('expectedComplex.txt'), 'utf8');
-/* const pathToFile5 = getFixturePath('fileComplex1.json');
-const pathToFile6 = getFixturePath('fileComplex2.json');
-test('Comparison of complex json files', () => {
-  expect(genDiff(pathToFile5, pathToFile6)).toBe(expectedComplex);
-});
-
-const pathToFile7 = getFixturePath('fileComplex1.yml');
-const pathToFile8 = getFixturePath('fileComplex2.yml');
-test('Comparison of complex yml files', () => {
-  expect(genDiff(pathToFile7, pathToFile8)).toBe(expectedComplex);
-}); */
 
 const expectedComplexPlain = readFileSync(getFixturePath('expectedComplexPlain.txt'), 'utf8');
-/* test('Comparison of complex json files with PLAIN formatter', () => {
-  expect(genDiff(pathToFile5, pathToFile6, 'plain')).toBe(expectedComplexPlain);
-}); */
 
 const expectedComplexJSON = readFileSync(getFixturePath('expectedComplexJSON.txt'), 'utf8');
-/* test('Comparison of complex json files with JSON formatter', () => {
-  expect(genDiff(pathToFile5, pathToFile6, 'json')).toBe(expectedComplexJSON);
-}); */
 
 const extensions = [['json'], ['yml'], ['plain']];
 
